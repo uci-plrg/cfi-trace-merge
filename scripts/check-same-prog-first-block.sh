02004ebf9bca84f3da880565fd160e0b614a44d9
Peizhao Ou <peizhaoo@uci.edu>
2013-03-01 01:30:44 -0800

    add the test for many runs of the same programs, seems that same programs always produce the same first main block!!

---
+++ scripts/check-same-prog-first-block.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+. check-fist-main-block
+# param: $1 should be the directory that contains all the runs
+#        of a single program
+function check-same-prog-first-block {
+	for run in $(ls $1); do
+		
+	done
+}
